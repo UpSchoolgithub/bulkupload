4c43c6421b66f02f3f57392c9e341f98ef980cfb
Bhavya <datamanagement@up.school>
2025-04-16 08:42:11 +0530

    fb

--- Backend/app.py
+++ Backend/app.py
@@ -558,7 +558,7 @@ def upload_file():
     )
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+app.run(debug=True, host='0.0.0.0', port=5000)
 
 @app.route("/", methods=["GET"])
 def home():
