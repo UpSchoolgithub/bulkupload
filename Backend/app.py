44610bd6e8c14093b2b4fc4fbb7b519ca2a47391
Bhavya <datamanagement@up.school>
2025-04-16 08:46:26 +0530

    ggd

--- Backend/app.py
+++ Backend/app.py
@@ -556,10 +556,11 @@ def upload_file():
         as_attachment=True,
         download_name='final_output.xlsx'
     )
-
-if __name__ == '__main__':
-app.run(debug=True, host='0.0.0.0', port=5000)
-
 @app.route("/", methods=["GET"])
 def home():
-    return "✅ Backend is up and working!"
+    return " Backend is up and working!"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+
